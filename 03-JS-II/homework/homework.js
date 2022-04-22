0464208c03dc18e8302e92187c4b21dbb0bf463a
lennycba <mario_lujan88@hotmail.com>
2022-04-22 16:31:27 -0300

    JSII guardado 1

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -147,7 +147,11 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  
+  if (numero < 2){
+    return false;
+  }else if (numero === 2){
+    return true;
+  }
 }
 
 function esVerdadero(valor){
