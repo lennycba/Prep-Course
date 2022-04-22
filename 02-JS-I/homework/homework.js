62ab2dcff65de5882d358927e5b351d20f031522
lennycba <mario_lujan88@hotmail.com>
2022-04-21 21:38:43 -0300

    revision 2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -182,9 +182,9 @@ function esPositivo(numero) {
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
   if (numero > 0){
-    return "es positivo";
+    return "Es positivo";
   }else if (numero < 0){
-    return "es negativo";
+    return "Es negativo";
   }else {
     return false;
   }
