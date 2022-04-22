648917c1b28a62b7a2ff8665dfe026eedde60a25
lennycba <mario_lujan88@hotmail.com>
2022-04-22 18:05:27 -0300

    JSIII ejercicios

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -26,6 +26,10 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  var nuevoarray = [];
+  for (var i=0 ; i < array.length ; i++){
+    nuevoarray [i] = array [i] + 1;
+  }return nuevoarray;
 }
 
 
