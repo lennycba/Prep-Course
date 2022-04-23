33cc264967349dd32dfcc7e6e1480b3352306599
lenny <mario_lujan88@hotmail.com>
2022-04-23 01:12:20 -0300

    guardado JSIII

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -115,7 +115,7 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
-  var multiplo = 0;
+  var multiplo = 1;
   if (arguments.length < 1){
       return 0;
   }else {
