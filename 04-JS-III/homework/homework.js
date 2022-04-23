32d7856d331abfd5f1b47c72e381532b210d2cd7
lenny <mario_lujan88@hotmail.com>
2022-04-23 01:18:06 -0300

    guardado JSIII

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -129,7 +129,7 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-return arreglo.length -1;
+return (arreglo.length -1);
 }
 
 
