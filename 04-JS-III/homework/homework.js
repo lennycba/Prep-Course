dc0dacc271c2811141d995ce29f5244f4c79614f
lenny <mario_lujan88@hotmail.com>
2022-04-23 00:37:15 -0300

    guardado JSIII

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -78,7 +78,7 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
-  var suma = 0
+  var suma = 0;
   for (i = 0 ; i < numeros.length ; i++){
     suma = suma + numeros [i];
   }return suma;
@@ -90,6 +90,11 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  var promedio = 0;
+  for (i = 0 ; i < resultadosTest.length ; i++){
+    promedio = (promedio + resultadosTest [i])
+  } var promedio = promedio / resultadosTest -1;
+  return promedio;
 }
 
 
