100dce717a77c65422076d32b56d8b1b320c3301
lenny <mario_lujan88@hotmail.com>
2022-04-23 02:52:41 -0300

    guardado JSIII

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -240,9 +240,8 @@ function continueStatement(numero) {
   let nuevoArray = [];
   var suma = numero;
     for(i = 0 ; i > 10 ; i++){
-        if (suma === 5){
-          continue;
-        }else {
+        if (suma === 5) continue;
+          else {
           suma = suma + 2;
           nuevoArray.push (suma);
         }
