d9fa551d86438218765399e8181ddd127722e6de
lenny <mario_lujan88@hotmail.com>
2022-04-23 02:33:13 -0300

    guardado JSIII

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -186,7 +186,7 @@ function mesesDelAño(array) {
       }
     }
     if (nuevoArray.length < 3){
-        return "No se encontraron los meses perdidos";
+        return "No se encontraron los meses pedidos";
   }else {
     return nuevoArray;
   }
