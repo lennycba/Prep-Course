370b083b4325126968254f7b84e807a3039bbad1
lennycba <mario_lujan88@hotmail.com>
2022-04-28 17:43:22 -0300

    repaso JSII

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -145,13 +145,20 @@ function esPrimo(numero) {
     return true
   }else if (numero < 2){
     return false;
-  }
+  }for (i = 2 ; i < numero ; i++){
+    if (numero % i === 0){
+      return false;
+    }
+  }return true;
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if ( valor === true){
+    return "Soy verdadero";
+  }return "Soy falso";
 
 }
 
@@ -168,14 +175,22 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if (numero > 99 && numero < 1000){
+    return true;
+  }return false;
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-
+ var a = numero;
+ var i = 0;
+ do {
+   i ++
+   a += 5;
+ }while (i < 8);
+ return a;
 }
 
 
