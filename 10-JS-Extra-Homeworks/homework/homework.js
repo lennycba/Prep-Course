fcf85d5e73e4f1abfcc00cc993c6e853808e2fca
lenny <mario_lujan88@hotmail.com>
2022-05-08 12:34:10 -0300

    ejercicios extras

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,8 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var arr = Object.entries(objeto);
+return(arr);
 }
 
 
@@ -18,7 +20,17 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  let obj = {};
+
+  for(let i = 0; i < string.length; i++){
+    if(!obj[string[i]]){
+      obj[string[i]] = 0;
+    }  
+    obj[string[i]] += 1;
+  }
+  return obj;
 }
+
 
 
 function capToFront(s) {
@@ -26,6 +38,15 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var mayuscula = '';
+  var minuscula = '';
+  for (i = 0; i < s.length; i++){
+
+    if(s[i] === s[i].toUpperCase()) mayuscula = mayuscula + s[i]
+      else minuscula= minuscula + s[i];
+  }
+
+  return mayuscula+minuscula;
 }
 
 
@@ -35,6 +56,12 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+ 
+  var espejo = '';
+  for(let i = str.length -1; i >= 0; i--) {
+    espejo += str[i];
+  };
+ return espejo.split(' ').reverse().join(' ');
 } 
 
 
